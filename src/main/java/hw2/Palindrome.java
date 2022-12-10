7b2a582b54153f1aa175f91978e76589c5c45027
Alexey <alexh2@yandex.ru>
2022-12-10 19:24:12 +0300

    hw2

--- src/main/java/hw2/Palindrome.java
+++ src/main/java/hw2/Palindrome.java
@@ -1,5 +1,5 @@
 package hw2;
-
+/*Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).*/
 public class Palindrome {
     public static boolean isPalindrome(String text) {
         String replaced = text.toLowerCase().replaceAll("[^A-Za-zА-Яа-я0-9]", "");
