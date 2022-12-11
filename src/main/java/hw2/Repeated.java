d15d9ee85e74567b9c59051f43e1391b2de03963
Alexey <alexh2@yandex.ru>
2022-12-11 16:01:36 +0300

    hw2

--- src/main/java/hw2/Repeated.java
+++ src/main/java/hw2/Repeated.java
@@ -1,6 +1,7 @@
 package hw2;
 /*Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
 который запишет эту строку в простой текстовый файл, обработайте исключения.*/
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,9 +11,9 @@ public class Repeated {
         String str = "TEST";
         String repeated = str.repeat(100);
         repeated.equals("");
-            System.out.print(repeated);
-            String content = repeated;
-            String path = "repeated.txt";
-            Files.write( Paths.get(path), content.getBytes());
-        }
+        System.out.print(repeated);
+        String content = repeated;
+        String path = "repeated.txt";
+        Files.write(Paths.get(path), content.getBytes());
     }
+}
