6cfc749ed6197620086a25e1c9131eeb40366b6e
Alexey <alexh2@yandex.ru>
2022-12-20 13:37:56 +0300

    hw4

---
+++ src/main/java/hw4/task1.java
@@ -0,0 +1,31 @@
+package hw4;
+
+import java.util.LinkedList;
+
+// 1. Пусть дан LinkedList с несколькими элементами.
+// Реализуйте метод, который вернет “перевернутый” список.
+
+public class task1 {
+
+    public static void main(String[] args) {
+
+        int[] listMas = {7, 6, 5, 6, 4, 3, 2, 1, 0};
+        LinkedList<Integer> linkList = new LinkedList<>();
+
+        for (Integer temp : listMas) {
+            linkList.add(temp);
+        }
+
+        int temp = 0;
+        while (temp != linkList.size() - 1) {
+            linkList.add(linkList.size() - temp, linkList.getFirst());
+            linkList.removeFirst();
+            temp++;
+        }
+
+
+        System.out.println("linkList = " + linkList);
+
+
+    }
+}
